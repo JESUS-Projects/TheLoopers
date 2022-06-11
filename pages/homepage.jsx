59dbdb5813cad1a5375fae5dd9b6e28f37f0d249
SINUT WATTANRPORN <s_w.piepie@outlook.com.au>
2022-06-11 23:04:47 +0700

    feat: add nav footer homepage

---
+++ pages/homepage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Navbar from "../components/common/Navbar";
+import Footer from "../components/common/Footer";
+
+export default function Homepage() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Hello Homepage</h1>
+      <Footer />
+    </div>
+  );
+}
