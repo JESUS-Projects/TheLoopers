e3945229102dd07469791923c4ae83a2b080477c
nipphittt17 <nipphit.api@student.mahidol.ac.th>
2022-06-13 20:28:24 +0700

    feat: update homepage

--- components/common/MainSearchBox.jsx
+++ components/common/MainSearchBox.jsx
@@ -6,12 +6,12 @@ export default function MainSearchBox() {
 
   return (
     <form action="POST">
-      <button type="submit">
-        <SearchIcon color="primary" fontSize="large" />
+      <button type="submit" className="text-primaryPink">
+        <SearchIcon sx={{ fontSize: 50 }}  />
       </button>
       <input
         placeholder="คุณต้องการทำอะไร"
-        className="ml-2 border border-primaryPink rounded-full py-1 px-4 w-80  placeholder:text-flamingoPink"
+        className="ml-2 border border-primaryPink rounded-full py-1 px-4 w-96 h-10 placeholder:text-flamingoPink"
         onChange={(e) => setInputVal(e.target.value)}
       />
     </form>
