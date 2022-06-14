354771833f6fc9bb4f5ef99eac9e518e66d815d3
SINUT WATTANRPORN <s_w.piepie@outlook.com.au>
2022-06-14 15:51:52 +0700

    feat: moves files

--- components/FilterSearchBox.jsx
+++ components/FilterSearchBox.jsx
@@ -5,14 +5,14 @@ export default function FilterSearchBox() {
   const [inputVal, setInputVal] = useState("");
 
   return (
-    <form action="POST">
+    <form action="POST" className="flex justify-center items-center">
       <input
         placeholder="ค้นหาด้วยคีย์เวิร์ด"
-        className="ml-2 border border-primaryPink rounded-full py-1 px-4 w-44 h-12 placeholder:text-flamingoPink"
+        className="border border-primaryPink rounded-full py-1 px-4 w-44 h-12 placeholder:text-flamingoPink"
         onChange={(e) => setInputVal(e.target.value)}
       />
       <button type="submit" className="text-primaryPink ml-2">
-        <SearchIcon sx={{ fontSize: 50 }}  />
+        <SearchIcon sx={{ fontSize: 50 }} />
       </button>
     </form>
   );
